a433ad007bb9ba8c28dd4b7c297c7597a34bb2fd
aria198a <aria198a@gmail.com>
2023-09-08 16:25:21 +0800

    modified

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-week3' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-week4' : '/',
   plugins: [react()],
 })
